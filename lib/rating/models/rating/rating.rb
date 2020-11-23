dcbe0d2d0d8298b17cce8e8bc76114a105ac1600
Duy Nguyen <truongduyng@users.noreply.github.com>
2020-11-23 21:15:55 +0700

    fix sql

--- lib/rating/models/rating/rating.rb
+++ lib/rating/models/rating/rating.rb
@@ -28,7 +28,7 @@ module Rating
 
         sql = %(
           SELECT
-            (CAST(#{total_count} AS DECIMAL(17, 14)) / #{distinct_count}) count_avg,
+            (CAST(#{total_count} AS DECIMAL(17, 4)) / #{distinct_count}) count_avg,
             COALESCE(AVG(value), 0)                                       rating_avg
           FROM #{rate_table_name}
           WHERE
